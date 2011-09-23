1cf6b4fe8438a932882656a0344a908f9e40a486
Doug Latornell <djl@douglatornell.ca>
2011-09-23 06:48:38 -0700

    Change tests to reflect refactoring of Config class.

--- bloomcast/tests/test_bloomcast.py
+++ bloomcast/tests/test_bloomcast.py
@@ -1,9 +1,9 @@
 """Unit tests for bloomcast modules.
 """
 from __future__ import absolute_import
-import unittest2 as unittest
 from mock import Mock
 from mock import DEFAULT
+import unittest2 as unittest
 
 
 class TestConfig(unittest.TestCase):
@@ -36,7 +36,19 @@ class TestConfig(unittest.TestCase):
         return mock_config_dict
 
 
-    def test_config_climate_url(self):
+    def _make_mock_infile_dict(self):
+        mock_infile_dict = {
+            'forcing_data_files': {
+                'air_temperature': None,
+                'relative_humidity': None,
+                'cloud_fraction': None,
+                'wind': None,
+            },
+        }
+        return mock_infile_dict
+
+
+    def test_load_config_climate_url(self):
         """load_config puts expected value in config.climate.url
         """
         test_url = 'http://example.com/climateData/bulkdata_e.html'
@@ -48,7 +60,7 @@ class TestConfig(unittest.TestCase):
         self.assertEqual(config.climate.url, test_url)
 
 
-    def test_config_climate_params(self):
+    def test_load_config_climate_params(self):
         """load_config puts expected value in config.climate.params
         """
         test_params = {
@@ -64,48 +76,54 @@ class TestConfig(unittest.TestCase):
         self.assertEqual(config.climate.params, test_params)
 
 
-    def test_config_climate_meteo_station_id(self):
-        """load_config puts expected value in config.climate.meteo.station_id
+    def test_load_meteo_config_station_id(self):
+        """_load_meteo_config puts expected value in config.climate.meteo.station_id
         """
         test_station_id = 889
         mock_config_dict = self._make_mock_config_dict()
         mock_config_dict['climate']['meteo']['station_id'] = test_station_id
+        mock_infile_dict = self._make_mock_infile_dict()
         config = self._make_one()
+        config.climate = Mock()
         config._read_yaml_file = Mock(return_value=mock_config_dict)
-        config.load_config('config_file')
+        config._load_meteo_config(mock_config_dict, mock_infile_dict)
         self.assertEqual(config.climate.meteo.station_id, test_station_id)
 
 
-    def test_config_climate_meteo_cloud_fraction_mapping(self):
-        """load_config puts expected value in cloud_fraction_mapping
+    def test_load_meteo_config_cloud_fraction_mapping(self):
+        """_load_meteo_config puts expected value in cloud_fraction_mapping
         """
         test_cloud_fraction_mapping_file = 'cloud_fraction_mapping.yaml'
         mock_config_dict = self._make_mock_config_dict()
         mock_config_dict['climate']['meteo']['cloud_fraction_mapping'] = (
             test_cloud_fraction_mapping_file)
+        mock_infile_dict = self._make_mock_infile_dict()
         test_cloud_fraction_mapping = {
             'Drizzle':   10,
             'Clear':  0,
         }
         config = self._make_one()
+        config.climate = Mock()
         def side_effect(config_file):
             return (DEFAULT if config_file == 'config_file'
                     else test_cloud_fraction_mapping)
         config._read_yaml_file = Mock(
             return_value=mock_config_dict, side_effect=side_effect)
-        config.load_config('config_file')
+        config._load_meteo_config(mock_config_dict, mock_infile_dict)
         self.assertEqual(
             config.climate.meteo.cloud_fraction_mapping,
             test_cloud_fraction_mapping)
 
 
-    def test_config_climate_wind_station_id(self):
-        """load_config puts expected value in config.climate.wind.station_id
+    def test_load_wind_config_station_id(self):
+        """_load_wind_config puts expected value in config.climate.wind.station_id
         """
         test_station_id = 889
         mock_config_dict = self._make_mock_config_dict()
         mock_config_dict['climate']['wind']['station_id'] = test_station_id
+        mock_infile_dict = self._make_mock_infile_dict()
         config = self._make_one()
+        config.climate = Mock()
         config._read_yaml_file = Mock(return_value=mock_config_dict)
-        config.load_config('config_file')
+        config._load_wind_config(mock_config_dict, mock_infile_dict)
         self.assertEqual(config.climate.wind.station_id, test_station_id)
