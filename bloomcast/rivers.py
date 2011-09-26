b0a90d780f2fa336918d61d05c29889c3a5fc9c0
Doug Latornell <djl@douglatornell.ca>
2011-09-26 14:09:00 -0700

    Continue development of river flows forcing data processor.

--- bloomcast/rivers.py
+++ bloomcast/rivers.py
@@ -2,8 +2,11 @@
 """
 from __future__ import absolute_import
 # Standard library:
+from datetime import date
 import logging
 import sys
+# HTTP Requests library:
+import requests
 # Bloomcast:
 from utils import Config
 
@@ -15,10 +18,12 @@ class RiversProcessor(object):
     """River flows forcing data processor.
     """
     def __init__(self, config):
-        data_readers = {'wind': self.read_river_flow}
+        self.config = config
+        self.data_readers = {'wind': self.read_river_flow}
+        self.hourlies = {}
 
 
-    def make_forcing_data_file(self):
+    def make_forcing_data_files(self):
         """
         """
         self.get_river_data()
@@ -27,6 +32,29 @@ class RiversProcessor(object):
     def get_river_data(self):
         """
         """
+        params = self.config.rivers.params
+        river = 'primary'
+        params['stn'] = getattr(self.config.rivers, river).station_id
+        params.update(self._date_params())
+        with requests.session() as s:
+            s.post(self.config.rivers.disclaimer_url,
+                   data=self.config.rivers.accept_disclaimer)
+            response = s.get(self.config.rivers.data_url, params=params)
+
+
+    def _date_params(self):
+        """
+        """
+        today = date.today()
+        params = {
+            'syr': today.year,
+            'smo': today.month,
+            'sday': 1,
+            'eyr': today.year,
+            'emo': today.month,
+            'eday': today.day - 1,
+        }
+        return params
 
 
     def read_river_flow(self):
