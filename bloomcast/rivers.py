047a352dd826728105e497de7a4bdcddd0500d57
Doug Latornell <djl@douglatornell.ca>
2011-09-28 20:57:25 -0700

    Add major & minor rivers to forcing data processor & hook it into bloomcast driver.

--- bloomcast/rivers.py
+++ bloomcast/rivers.py
@@ -27,18 +27,28 @@ class RiversProcessor(ForcingDataProcessor):
 
 
     def make_forcing_data_files(self):
+        """Get the river flows forcing data from the Environment
+        Canada WaterOffice website, process it to extract average
+        daily flow values from the HTML table, trim incomplete days
+        from the end, patch missing values, and write the data to
+        files in the format that SOG expects.
         """
-        """
-        self.get_river_data()
-        self.process_data('major')
+        for river in 'major minor'.split():
+            output_file = self.config.rivers.output_files[river]
+            file_obj = open(output_file, 'wt')
+            self.get_river_data(river)
+            self.process_data(river, end_date=self.config.data_date)
+            log.debug(
+                'latest {0} river flow {1}'.format(river, self.data[river][-1]))
+            file_obj.writelines(self.format_data(river))
 
 
-    def get_river_data(self):
-        """Return a BeautifulSoup parser object containing the
-        river flow data table Environment Canada WaterOffice page.
+    def get_river_data(self, river):
+        """Return a BeautifulSoup parser object containing the river
+        flow data table scraped from the Environment Canada
+        WaterOffice page.
         """
         params = self.config.rivers.params
-        river = 'major'
         params['stn'] = getattr(self.config.rivers, river).station_id
         params.update(self._date_params())
         with requests.session() as s:
@@ -46,11 +56,16 @@ class RiversProcessor(ForcingDataProcessor):
                    data=self.config.rivers.accept_disclaimer)
             response = s.get(self.config.rivers.data_url, params=params)
         soup = BeautifulSoup(response)
-        self.data = soup.find('table', id='dataTable')
+        self.raw_data = soup.find('table', id='dataTable')
 
 
     def _date_params(self):
-        """
+        """Return a dict of the components of today's date.
+
+        The keys are the component names in the format required for
+        requests to the :kbd:`wateroffice.gc.ca` site.
+
+        The values are today's date components as integers.
         """
         today = date.today()
         params = {
@@ -59,7 +74,7 @@ class RiversProcessor(ForcingDataProcessor):
             'sday': 1,
             'eyr': today.year,
             'emo': today.month,
-            'eday': 2,
+            'eday': today.day,
         }
         return params
 
@@ -68,7 +83,7 @@ class RiversProcessor(ForcingDataProcessor):
         """Process data from BeautifulSoup parser object to a list of
         hourly timestamps and data values.
         """
-        tds = self.raw_data[qty].findAll('td')
+        tds = self.raw_data.findAll('td')
         timestamps = (td.string for td in tds[::2])
         flows = (td.string for td in tds[1::2])
         data_day = self.read_datestamp(tds[0].string)
@@ -76,6 +91,8 @@ class RiversProcessor(ForcingDataProcessor):
         self.data[qty] = []
         for timestamp, flow in izip(timestamps, flows):
             datestamp = self.read_datestamp(timestamp)
+            if datestamp > end_date:
+                break
             if datestamp == data_day:
                 flow_sum += float(flow)
                 count += 1
@@ -84,8 +101,7 @@ class RiversProcessor(ForcingDataProcessor):
                 data_day = datestamp
                 flow_sum = float(flow)
                 count = 1
-        else:
-            self.data[qty].append((data_day, flow_sum / count))
+        self.data[qty].append((data_day, flow_sum / count))
 
 
     def read_datestamp(self, string):
@@ -123,6 +139,7 @@ def run(config_file):
     logging.basicConfig(level=logging.DEBUG)
     config = Config()
     config.load_config(config_file)
+    config.data_date = date.today()
     rivers = RiversProcessor(config)
     rivers.make_forcing_data_files()
 
