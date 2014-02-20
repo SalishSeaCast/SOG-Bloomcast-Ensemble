d47d25eb907a3c06311b308349b81de388c5f071
Doug Latornell <djl@douglatornell.ca>
2014-02-20 08:58:48 -0800

    Change comment style of license notice.

--- cf_analysis/cf_analysis.py
+++ cf_analysis/cf_analysis.py
@@ -1,3 +1,17 @@
+# Copyright 2011-2014 Doug Latornell and The University of British Columbia
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Special use script to help with cloud fraction algorithm improvement.
 
 Code from other bloomcast modules is repeated here rather than implementing
@@ -35,21 +49,6 @@ The algorithm she decided on is:
 
   * weather description strings as keys
   * arrays of either 1 or 12 cloud fraction values as values
-
-
-Copyright 2011-2014 Doug Latornell and The University of British Columbia
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 """
 import contextlib
 from cStringIO import StringIO
