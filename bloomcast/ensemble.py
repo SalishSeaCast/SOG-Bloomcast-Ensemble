58875cb3a24cd0dfbb48322df5eab82db72e5dc2
Doug Latornell <djl@douglatornell.ca>
2014-03-19 17:31:10 +0100

    Start implementation of time series plots for ensemble results page.

--- bloomcast/ensemble.py
+++ bloomcast/ensemble.py
@@ -38,6 +38,15 @@ from . import (
 __all__ = ['Ensemble']
 
 
+# Colours for plots
+COLORS = {
+    'axes': '#ebebeb',     # bootswatch superhero theme text
+    'bg': '#2B3E50',       # bootswatch superhero theme background
+    'diatoms': '#7CC643',  # yellow-green that looks good on the background
+    'nitrate': '#82AFDC',  # blue-green that looks good on the background
+}
+
+
 class Ensemble(cliff.command.Command):
     """run the ensemble bloomcast
     """
@@ -104,6 +113,8 @@ class Ensemble(cliff.command.Command):
         self._load_biology_timeseries()
         prediction, bloom_dates = self._calc_bloom_dates()
         self._load_physics_timeseries(prediction)
+        timeseries_plots = self._create_timeseries_graphs(
+            COLORS, prediction, bloom_dates)
 
     def _create_infile_edits(self):
         """Create YAML infile edit files for each ensemble member SOG run.
@@ -300,6 +311,41 @@ class Ensemble(cliff.command.Command):
         self.wind.calc_mpl_dates(self.config.run_start_date)
         self.log.debug(
             'read wind speed forcing timeseries from {}'.format(filename))
+
+    def _create_timeseries_graphs(self, colors, prediction, bloom_dates):
+        """Create time series plot figure objects.
+        """
+        import ipdb; ipdb.set_trace()
+        timeseries_plots = {
+            'nitrate_diatoms': visualization.nitrate_diatoms_timeseries(
+                self.nitrate_ts,
+                self.diatoms_ts,
+                colors,
+                self.config.data_date,
+                prediction,
+                bloom_dates,
+                titles=('3 m Avg Nitrate Concentration [uM N]',
+                        '3 m Avg Diatom Biomass [uM N]'),
+            ),
+            'temperature_salinity': visualization.two_axis_timeseries(
+                self.temperature,
+                self.salinity,
+                colors,
+                self.config.data_date,
+                bloom_dates,
+                titles=('3 m Avg Temperature [deg C]',
+                        '3 m Avg Salinity [-]'),
+            ),
+            'mld_wind': visualization.mixing_layer_depth_wind_timeseries(
+                self.mixing_layer_depth,
+                self.wind,
+                colors,
+                self.config.data_date,
+                titles=('Mixing Layer Depth [m]',
+                        'Wind Speed [m/s]'),
+            ),
+        }
+        return timeseries_plots
 
 
 def configure_logging(config, bloom_date_log):
