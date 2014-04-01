ab61b4b9fcc37e261b26b7f42b9057b258ec50a5
Doug Latornell <djl@douglatornell.ca>
2014-04-01 07:53:09 +0200

    Change cron job script to run ensemble sub-command.

--- cronjob.sh
+++ cronjob.sh
@@ -3,6 +3,6 @@
 # make sure that this file has mode 744
 # and that MAILTO is set in crontab
 
-VENV=/data/dlatorne/.virtualenvs/bloomcast
-RUN_DIR=/data/dlatorne/SOG-projects/SoG-bloomcast/run
-. $VENV/bin/activate && cd $RUN_DIR && $VENV/bin/bloomcast config.yaml
+VENV=/data/dlatorne/.virtualenvs/bloomcast-ensemble
+RUN_DIR=/data/dlatorne/SOG-projects/SoG-bloomcast-ensemble/run
+. $VENV/bin/activate && cd $RUN_DIR && $VENV/bin/bloomcast ensemble -v config.yaml
