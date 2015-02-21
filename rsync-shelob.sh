85a3038e638db4751a463a69f094bdebde09c0e8
Doug Latornell <dlatornell@eos.ubc.ca>
2015-02-21 11:59:23 -0800

    Remove compression from rsync to shelob; versions are incompatible.

--- rsync-shelob.sh
+++ rsync-shelob.sh
@@ -2,5 +2,5 @@
 
 HTML_DIR=www/salishsea-site/site/_build/html
 SHELOB=shelob:/www/salishsea/data
-rsync -Rtvhz $HTML_DIR/./bloomcast/spring_diatoms.html $SHELOB
-rsync -rRtvhz $HTML_DIR/./_static/bloomcast/spring_diatoms/ $SHELOB
+rsync -Rtvh $HTML_DIR/./bloomcast/spring_diatoms.html $SHELOB
+rsync -rRtvh $HTML_DIR/./_static/bloomcast/spring_diatoms/ $SHELOB
