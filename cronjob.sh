d866ae82ca71b9f1b4380a17fb77543854b9da9a
Doug Latornell <djl@douglatornell.ca>
2016-01-24 17:11:22 -0800

    Update path to bloomcast conda environment.

--- cronjob.sh
+++ cronjob.sh
@@ -3,6 +3,6 @@
 # make sure that this file has mode 744
 # and that MAILTO is set in crontab
 
-CONDA_ENV=/home/dlatorne/anaconda/envs/bloomcast
+CONDA_ENV=/home/dlatorne/miniconda3/envs/bloomcast
 RUN_DIR=/data/dlatorne/SOG-projects/SoG-bloomcast-ensemble/run
 source $CONDA_ENV/bin/activate bloomcast && cd $RUN_DIR && $CONDA_ENV/bin/bloomcast ensemble -v config.yaml
