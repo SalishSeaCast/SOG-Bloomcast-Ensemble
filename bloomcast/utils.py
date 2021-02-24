8ad2c8060d056423db2e50a0b9d08219eb159939
Doug Latornell <douglatornell@eoas.ubc.ca>
2021-02-24 15:23:53 -0800

    Revert 2017 rev f239d8cd re: parsing ECCC XML
    Garbage characters at start of text (probably 3 byte BOM) that appeared in 2017
    are gone in 2021!

--- bloomcast/utils.py
+++ bloomcast/utils.py
@@ -307,7 +307,7 @@ class ClimateDataProcessor(ForcingDataProcessor):
             self.config.climate, data_type).station_id
         params.update(self._date_params(data_month))
         response = requests.get(self.config.climate.url, params=params)
-        tree = ElementTree.parse(io.StringIO(response.text[3:]))
+        tree = ElementTree.parse(io.StringIO(response.text))
         root = tree.getroot()
         self.raw_data.extend(root.findall('stationdata'))
 
