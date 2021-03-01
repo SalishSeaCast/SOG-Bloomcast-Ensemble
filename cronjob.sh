dc2a4114b537b72f6111c2e0af031f838bf7acbc
Doug Latornell <douglatornell@eoas.ubc.ca>
2021-03-01 10:05:24 -0800

    Update conda env path in cronjob script

--- cronjob.sh
+++ cronjob.sh
@@ -4,7 +4,7 @@
 # and that MAILTO is set in crontab
 
 CONDA_BASE_ENV=/home/dlatorne/miniconda3/
-CONDA_BLOOMCAST_ENV=/data/dlatorne/SOG-projects/blomcast-env-mpl-1.5.3
+CONDA_BLOOMCAST_ENV=/home/dlatorne/conda_envs/bloomcast
 RUN_DIR=/data/dlatorne/SOG-projects/SOG-Bloomcast-Ensemble/run
 source $CONDA_BASE_ENV/bin/activate $CONDA_BLOOMCAST_ENV && \
     cd $RUN_DIR && \
