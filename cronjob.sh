0fdf24e8b700cd2fbf77bd5f59f667e586b23efe
Doug Latornell <dlatornell@eoas.ubc.ca>
2022-03-05 10:44:59 -0800

    Update conda installation path in cron job script
    re: change from miniconda3 to mambaforge-pypy3.

--- cronjob.sh
+++ cronjob.sh
@@ -3,7 +3,7 @@
 # make sure that this file has mode 744
 # and that MAILTO is set in crontab
 
-CONDA_BASE_ENV=/home/dlatorne/miniconda3/
+CONDA_BASE_ENV=/home/dlatorne/mambaforge-pypy3
 CONDA_BLOOMCAST_ENV=/home/dlatorne/conda_envs/bloomcast
 RUN_DIR=/data/dlatorne/SOG-projects/SOG-Bloomcast-Ensemble/run
 source $CONDA_BASE_ENV/bin/activate $CONDA_BLOOMCAST_ENV && \
