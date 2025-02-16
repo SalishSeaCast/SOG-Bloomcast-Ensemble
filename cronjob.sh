8fc168d80ed57e6dcf71399ab9f083765a3bfff6
Doug Latornell <dlatornell@eoas.ubc.ca>
2025-02-16 11:01:35 -0800

    Update Conda base env path in cronjob script
    Changed the conda base environment path from mambaforge-pypy3 to miniforge-pypy3
    to align with recent changes in the environment setup.

--- cronjob.sh
+++ cronjob.sh
@@ -3,9 +3,9 @@
 # make sure that this file has mode 744
 # and that MAILTO is set in crontab
 
-CONDA_BASE_ENV=/home/dlatorne/mambaforge-pypy3
+CONDA_BASE_ENV=/home/dlatorne/miniforge-pypy3
 CONDA_BLOOMCAST_ENV=/home/dlatorne/conda_envs/bloomcast
 RUN_DIR=/data/dlatorne/SOG-projects/SOG-Bloomcast-Ensemble/run
-source $CONDA_BASE_ENV/bin/activate $CONDA_BLOOMCAST_ENV && \
-    cd $RUN_DIR && \
-    $CONDA_BLOOMCAST_ENV/bin/bloomcast ensemble -v config.yaml --debug
+$CONDA_BASE_ENV/bin/activate $CONDA_BLOOMCAST_ENV && \
+  cd $RUN_DIR && \
+  $CONDA_BLOOMCAST_ENV/bin/bloomcast ensemble -v config.yaml --debug
